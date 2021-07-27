a617f429c232c48374f4d5e59765223869e3fe73
今村 昌平 <s.imamura@ef-4.co.jp>
2021-07-27 20:06:31 +0900

    add composition api

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <script>
+import { defineComponent, ref } from '@vue/composition-api'
 
+export default defineComponent({
+  setup() {
+    const pageName = ref('用語集一覧')
+    return {
+      pageName,
+    }
+  },
+})
 </script>
 
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center"> 用語集一覧 </v-card>
+      <v-card class="logo py-4 d-flex justify-center"> {{ pageName }} </v-card>
       <v-card> </v-card>
     </v-col>
   </v-row>
